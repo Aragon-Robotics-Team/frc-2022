1ef7c6db3bd4b302ba240f3307c2d5f62b7fa3d5
Nicole Chen <nicolechen@Nicoles-MacBook-Air.local>
2022-01-25 17:31:36 -0800

    initial pixy code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.commands.driving.ArcadeDrive;
+import frc.robot.commands.driving.testing.TestPixy;
+import frc.robot.sensors.PixyCamera;
 import edu.wpi.first.wpilibj.Joystick;
 
 /**
@@ -28,6 +30,9 @@ public class RobotContainer {
   private final Drivetrain m_drivetrain = new Drivetrain();
   private final Joystick m_joystick = new Joystick(Config.kdriveJoystickPort);
   private final ArcadeDrive m_arcadeDrive = new ArcadeDrive(m_drivetrain, m_joystick);
+  private final PixyCamera m_pixy = new PixyCamera();
+
+  private final Command m_testPixy = new TestPixy(m_pixy);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -52,6 +57,8 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
+    m_pixy.setDefaultCommand(m_testPixy);
+
     return null;
   }
 
