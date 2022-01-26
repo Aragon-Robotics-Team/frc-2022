b0d71af52eeab6e0acec136365bf498b48033fdd
Satvik Reddy <reddy.satvik@gmail.com>
2022-01-25 16:25:14 -0800

    installed navx libraries

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,43 +3,37 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import io.github.pseudoresonance.pixy2api.Pixy2;
-
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.sensors.CANCoder;
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
   private static final class Config {
     // placeholders
     // actually channels (victors) / device nunmbers (talons)
-    public static final int kLeftMotorSlave = 0;
-    public static final int kRightMotorSlave = 1;
-    public static final int kLeftMotorMaster = 2;
+    public static final int kLeftMotorSlave = 7;
+    public static final int kRightMotorSlave = 8;
+    public static final int kLeftMotorMaster = 6;
     public static final int kRightMotorMaster = 3;
 
     public static final double kGearRatio = 1.0;
   }
   /*
+   * 
+   * talons = masters victors = slaves
+   * 
+   */
 
-  talons = masters
-  victors = slaves
-
-  */
-
-  // private CANSparkMax m_leftMotorSlave = new CANSparkMax(Config.kLeftMotorSlave, MotorType.kBrushless);
-  // private CANSparkMax m_rightMotorSlave = new CANSparkMax(Config.kRightMotorSlave, MotorType.kBrushless);
-  // private CANSparkMax m_leftMotorMaster = new CANSparkMax(Config.kLeftMotorMaster, MotorType.kBrushless);
-  // private CANSparkMax m_rightMotorMaster = new CANSparkMax(Config.kRightMotorMaster, MotorType.kBrushless);
+  // private CANSparkMax m_leftMotorSlave = new
+  // CANSparkMax(Config.kLeftMotorSlave, MotorType.kBrushless);
+  // private CANSparkMax m_rightMotorSlave = new
+  // CANSparkMax(Config.kRightMotorSlave, MotorType.kBrushless);
+  // private CANSparkMax m_leftMotorMaster = new
+  // CANSparkMax(Config.kLeftMotorMaster, MotorType.kBrushless);
+  // private CANSparkMax m_rightMotorMaster = new
+  // CANSparkMax(Config.kRightMotorMaster, MotorType.kBrushless);
 
   private VictorSPX m_leftMotorSlave = new VictorSPX(Config.kLeftMotorSlave);
   private VictorSPX m_rightMotorSlave = new VictorSPX(Config.kRightMotorSlave);
@@ -48,7 +42,7 @@ public class Drivetrain extends SubsystemBase {
 
   // make encoders for talon motors
 
-  //encoders: CTRE magnetic encoders :)
+  // encoders: CTRE magnetic encoders :)
 
   // private CANEncoder m_encRight = m_rightMotorMaster.getCANEncoder();
   // private CANEncoder m_encLeft = m_leftMotorMaster.getCANEnoder();
@@ -57,12 +51,10 @@ public class Drivetrain extends SubsystemBase {
 
   /** Creates a new Drivetrain. */
   public Drivetrain() {
-    // Inverts masters
-    m_leftMotorMaster.setInverted(true);
-    m_rightMotorMaster.setInverted(true);
+    m_leftMotorSlave.setInverted(true);
+    m_rightMotorSlave.setInverted(true);
 
     // Sets following
-    
     m_leftMotorSlave.follow(m_leftMotorMaster);
     m_rightMotorSlave.follow(m_rightMotorMaster);
 
@@ -70,8 +62,7 @@ public class Drivetrain extends SubsystemBase {
     // m_encRight.setPositionConversionFactor(1.0);
   }
 
-
-  //this is unused
+  // this is unused
   public void stopMotors() {
     m_leftMotorMaster.stopMotor();
     m_rightMotorMaster.stopMotor();
@@ -85,12 +76,12 @@ public class Drivetrain extends SubsystemBase {
   }
 
   public double position() {
-    //distance traveled
-    
-    // rEncoder = right encoder
-    //encoders have not yet been defined soooooooooooooo
+    // distance traveled
 
-    //will need another variable here for spins to feet (so we can read it)
+    // rEncoder = right encoder
+    // encoders have not yet been defined soooooooooooooo
+
+    // will need another variable here for spins to feet (so we can read it)
     return m_leftMotorMaster.getSelectedSensorPosition() * Config.kGearRatio;
   }
 
@@ -100,8 +91,6 @@ public class Drivetrain extends SubsystemBase {
   }
 
   public DifferentialDrive getDrive() {
-
     return m_drive;
-
   }
 }
