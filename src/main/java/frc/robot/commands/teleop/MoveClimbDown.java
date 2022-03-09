22dbfb61d6bb1e6088f943a8605ad6e0ad327b7e
Pranav <24vyasp93@stu.smuhsd.org>
2022-03-08 16:19:17 -0800

    Added code for the climb

--- src/main/java/frc/robot/commands/teleop/MoveClimbDown.java
+++ src/main/java/frc/robot/commands/teleop/MoveClimbDown.java
@@ -5,11 +5,25 @@
 package frc.robot.commands.teleop;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Climb;
 
-public class TurnMotor extends CommandBase {
+
+public class MoveClimbDown extends CommandBase {
+  private static final class Config {
+    public static final double kSpeed = 0.0;
+    public static final double kTicks = 0.0; 
+   }
+
+  private Climb m_climb;
+  private double m_speed;
+
   /** Creates a new TurnMotor. */
-  public TurnMotor() {
+  public MoveClimbDown(Climb climb) {
     // Use addRequirements() here to declare subsystem dependencies.
+      m_climb = climb;
+      m_speed = Config.kSpeed;
+
+      addRequirements(climb);
   }
 
   // Called when the command is initially scheduled.
@@ -18,15 +32,21 @@ public class TurnMotor extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_climb.set(m_speed);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_climb.set(0.0);
+  }
 
   // Returns true when the command should end.
+
+  //Testing required to find the ticks at which the motor should stop
   @Override
-  public boolean isFinished() {
-    return false;
+  public boolean isFinished() { 
+    return m_climb.getEncoderValue() <= Config.kTicks;
   }
 }
